890b8f9c327f9740b1f726cded1591f18aa98c1b
Nguyễn Đăng Dũng <dangdungcntt@gmail.com>
2025-05-26 12:39:15 +0700

    Update entrypoint.sh

--- container-image-root/entrypoint.sh
+++ container-image-root/entrypoint.sh
@@ -3,9 +3,8 @@ set -x
 
 [ "$IP" == "" ] && IP=$(curl ifconfig.co/ip -s)
 
-service ntp start
-curl -s https://core.telegram.org/getProxySecret -o /srv/MTProxy/objs/bin/proxy-secret > /dev/null 2>&1
-curl -s https://core.telegram.org/getProxyConfig -o /srv/MTProxy/objs/bin/proxy-multi.conf > /dev/null 2>&1
+curl -s https://core.telegram.org/getProxySecret -o /srv/MTProxy/objs/bin/proxy-secret
+curl -s https://core.telegram.org/getProxyConfig -o /srv/MTProxy/objs/bin/proxy-multi.conf
 cron
 
 if [ "$1" == "" ];
